5756443cc28c7b667ae8611f1e9e7e065b97e43a
china3416 <80491732+china3416@users.noreply.github.com>
2021-03-18 19:29:06 +0300

    Update list1.py

--- list1.py
+++ list1.py
@@ -5,23 +5,145 @@ discord.Intents.all()
 import os
 
 
-bot = commands.Bot(intents = discord.Intents.all(), command_prefix="/", description="Test Bot for the discord.py")
+bot = commands.Bot(intents = discord.Intents.all(), command_prefix="-", description="Test Bot for the discord.py")
 
-count1 = 0
-count2 = 0
-i = 3
-y = 0
-z = 0
-team_id = 0
-x = 0
-open_check = False
 
-# список зарегестрированных команд
-team_name = []
+teams = []
+reserve = []
 black_list = []
+
+teams_count = 0
+role_count = 0
+i = 3
+x = 0
+l = 0
+team_id = 0
+reg_check = False
+
 while x < 26:
-    team_name.append('-')
+    teams.append('-')
     x = x + 1
+
+
+@bot.command()
+@commands.has_role(817170720941015050)
+async def open_reg(ctx):
+    manager = discord.utils.get(ctx.guild.roles, name="Manager")
+    team_list_ch = bot.get_channel(816827996681666650)
+    reg_ch = bot.get_channel(819000355613835304)
+
+    global reg_check
+    global teams_count
+    global i
+    global x
+    global team_id
+
+    reg_check = True
+    teams_count = 0
+    i = 3
+    x = 0
+
+
+    while x < 26:
+        teams[x] = '-'
+        x = x + 1
+
+
+
+    team_list_msg = await team_list_ch.send('**Team List\n1. '+teams[1]+' \n2. '+teams[2]+' \n3. '+teams[3]+' \n4. '+teams[4]+' \n5. '+teams[5]+' \n6. '+teams[6]+' '
+                   '\n7. '+teams[7]+'\n8. '+teams[8]+'\n9. '+teams[9]+'\n10. '+teams[10]+''
+                   '\n11. '+teams[11]+'\n12. '+teams[12]+'\n13. '+teams[13]+'\n14. '+teams[14]+''
+                   '\n15. '+teams[15]+'\n16. '+teams[16]+'\n17. '+teams[17]+'\n18. '+teams[18]+''
+                   '\n19. '+teams[19]+'\n20. '+teams[20]+'\nReserve\n1. '+teams[21]+''
+                   '\n2. '+teams[22]+'\n3. '+teams[23]+'\n4. '+teams[24]+'\n5. '+teams[25]+'**')
+
+    team_id = team_list_msg.id
+
+    await reg_ch.send("**Регистрация открыта! @everyone**")
+    await reg_ch.set_permissions(manager, read_messages=True, send_messages=True)
+    await ctx.message.delete()
+
+
+
+@bot.command()
+async def reg(ctx, team_name: str, member: discord.Member):
+    team_list_ch = bot.get_channel(816827996681666650)
+    reg_ch = bot.get_channel(819000355613835304)
+
+    cap = discord.utils.get(ctx.guild.roles, name="Cap-chat")
+    manager = discord.utils.get(ctx.guild.roles, name="Manager")
+
+    global teams_count
+    global i
+
+
+    if ctx.channel.id == 819000355613835304 and reg_check == True:
+        for item in black_list:
+
+            if item == ctx.author:
+                await ctx.message.add_reaction("<:No:818987499766546432>")
+                return
+
+        if ctx.author == bot.user:
+            return
+
+        if teams_count < 18:
+            await ctx.message.add_reaction("<:Yes:818988469891629087>")
+            await member.add_roles(cap)
+
+        if teams_count > 17 and teams_count < 23:
+            await ctx.message.add_reaction("<:Res:818992166906888233>")
+            reserve.append(member)
+
+        if teams_count < 23:
+            teams[i] = team_name + ' ' + member.mention
+
+            i = i + 1
+            teams_count = teams_count + 1
+            team_list_edit = await team_list_ch.fetch_message(team_id)
+            await team_list_edit.edit(content='**Team List\n1. '+teams[1]+' \n2. '+teams[2]+' \n3. '+teams[3]+' \n4. '+teams[4]+' \n5. '+teams[5]+' \n6. '+teams[6]+' '
+                                              '\n7. '+teams[7]+'\n8. '+teams[8]+'\n9. '+teams[9]+'\n10. '+teams[10]+''
+                                              '\n11. '+teams[11]+'\n12. '+teams[12]+'\n13. '+teams[13]+'\n14. '+teams[14]+''
+                                              '\n15. '+teams[15]+'\n16. '+teams[16]+'\n17. '+teams[17]+'\n18. '+teams[18]+''
+                                              '\n19. '+teams[19]+'\n20. '+teams[20]+'\nReserve\n1. '+teams[21]+''
+                                              '\n2. '+teams[22]+'\n3. '+teams[23]+'\n4. '+teams[24]+'\n5. '+teams[25]+'**')
+
+
+
+        if teams_count > 22:
+            embed_obj = discord.Embed(description='**Регистрация закрыта! @everyone**', colour = discord.Color.from_rgb(199, 0, 0))
+            await ctx.send(embed=embed_obj)
+            await reg_ch.set_permissions(manager, read_messages=True, send_messages=False)
+
+
+
+
+@bot.command()
+async def unreg(ctx, team_name: str, member: discord.Member):
+    team_list_ch = bot.get_channel(816827996681666650)
+    cap = discord.utils.get(ctx.guild.roles, name="Cap-chat")
+
+    global i
+    global l
+
+    if ctx.channel.id == 819000355613835304 and reg_check == True:
+        teams.remove(team_name + ' ' + member.mention)
+        await member.remove_roles(cap)
+        teams.append('-')
+
+        i = i - 1
+
+        if teams_count > 17:
+            await reserve[l].add_roles(cap)
+            l += 1
+
+        team_list_edit = await team_list_ch.fetch_message(team_id)
+        await team_list_edit.edit(content='**Team List\n1. '+teams[1]+' \n2. '+teams[2]+' \n3. '+teams[3]+' \n4. '+teams[4]+' \n5. '+teams[5]+' \n6. '+teams[6]+' '
+                                          '\n7. '+teams[7]+'\n8. '+teams[8]+'\n9. '+teams[9]+'\n10. '+teams[10]+''
+                                          '\n11. '+teams[11]+'\n12. '+teams[12]+'\n13. '+teams[13]+'\n14. '+teams[14]+''
+                                          '\n15. '+teams[15]+'\n16. '+teams[16]+'\n17. '+teams[17]+'\n18. '+teams[18]+''
+                                          '\n19. '+teams[19]+'\n20. '+teams[20]+'\nReserve\n1. '+teams[21]+''
+                                          '\n2. '+teams[22]+'\n3. '+teams[23]+'\n4. '+teams[24]+'\n5. '+teams[25]+'**')
 
 
 
@@ -30,28 +152,21 @@ while x < 26:
 @bot.command()
 @commands.has_role(817170720941015050)
 async def clear_role(ctx, *, name_role: str):
-    global y
-    y = 0
+    global role_count
+    role_count = 0
     role = discord.utils.get(ctx.guild.roles, name=name_role)
     cap = role.members
-    x = len(cap)
-    while y <= x - 1:
-        await cap[y].remove_roles(role)
-        y = y + 1
-        
+    member_length = len(cap)
+    while role_count <= member_length - 1:
+        await cap[role_count].remove_roles(role)
+        role_count += 1
     await ctx.message.delete()
-
-
-
 
 # черный список
 @bot.command()
 @commands.has_role(817170720941015050)
 async def b_list(ctx, member: discord.Member):
     black_list.append(member)
-    await ctx.message.delete()
-
-
 
 
 # "белый" список
@@ -61,50 +176,26 @@ async def w_list(ctx, member: discord.Member):
     for item in black_list:
         if item == member:
             black_list.remove(member)
-            
-    await ctx.message.delete()
 
 
 
 
-
-#Открытие регистрации
 @bot.command()
 @commands.has_role(817170720941015050)
-async def open_reg(ctx):
-    global team_id
-    global count1
-    global count2
-    global i
-    global x
-    global open_check
-    x = 0
-    count1 = 0
-    count2 = 0
-    i = 3
-    open_check = True
-    channel = bot.get_channel(819000355613835304)
-    team_list_ch = bot.get_channel(816827996681666650)
-    Manager = discord.utils.get(ctx.guild.roles, name="Manager")
-    
-    while x < 26:
-        team_name[x] = '-'
-        x = x + 1
+async def close_reg(ctx):
+    global reg_check
 
-   
-    msg_teamList = await team_list_ch.send('**Team List\n1. '+team_name[1]+' \n2. '+team_name[2]+' \n3. '+team_name[3]+' \n4. '+team_name[4]+' \n5. '+team_name[5]+' \n6. '+team_name[6]+' '
-                                             '\n7. '+team_name[7]+'\n8. '+team_name[8]+'\n9. '+team_name[9]+'\n10. '+team_name[10]+''
-                                             '\n11. '+team_name[11]+'\n12. '+team_name[12]+'\n13. '+team_name[13]+'\n14. '+team_name[14]+''
-                                             '\n15. '+team_name[15]+'\n16. '+team_name[16]+'\n17. '+team_name[17]+'\n18. '+team_name[18]+''
-                                             '\n19. '+team_name[19]+'\n20. '+team_name[20]+'\nReserve\n1. '+team_name[21]+''
-                                             '\n2. '+team_name[22]+'\n3. '+team_name[23]+'\n4. '+team_name[24]+'\n5. '+team_name[25]+'**')
-
-    team_id = msg_teamList.id
-
-
-    await channel.send("**Регистрация открыта! @everyone**")
-    await channel.set_permissions(Manager, read_messages=True, send_messages=True)
+    reg_ch = bot.get_channel(819000355613835304)
+    manager = discord.utils.get(ctx.guild.roles, name="Manager")
+    reg_check = False
+    embed_obj = discord.Embed(description='**Регистрация закрыта! @everyone**', colour = discord.Color.from_rgb(199, 0, 0))
+    await reg_ch.set_permissions(manager, read_messages=True, send_messages=False)
+    await reg_ch.send(embed=embed_obj)
     await ctx.message.delete()
+
+
+
+
 
 
 
@@ -130,18 +221,7 @@ async def lobby(ctx, map: str, time_m: str, id_m: str, passw: str):
 
 
 
-@bot.command()
-@commands.has_role(817170720941015050)
-async def close_reg(ctx):
-    reg_ch = bot.get_channel(819000355613835304)
-    channel = bot.get_channel(819000355613835304)
-    global open_check
-    open_check = False
-    embed_obj = discord.Embed(description='**Регистрация закрыта! @everyone**', colour = discord.Color.from_rgb(199, 0, 0))
 
-    await reg_ch.send(embed=embed_obj)
-    await message.channel.set_permissions(Manager, read_messages=True, send_messages=False)
-    await ctx.message.delete()
 
 # embed сообщение
 
@@ -164,101 +244,24 @@ async def send(ctx, *, text: str):
 async def team(ctx, i: int, *, name: str):
 
     if name == 'none':
-        name = '-'
-
-    team_name[i] = name
+        teams.pop(i)
+    else:
+        teams[i] = name
 
     team_list_ch = bot.get_channel(816827996681666650)
     team_list_edit = await team_list_ch.fetch_message(team_id)
-    await team_list_edit.edit(content='**Team List \n1. '+team_name[1]+' \n2. '+team_name[2]+' \n3. '+team_name[3]+' \n4. '+team_name[4]+' \n5. '+team_name[5]+' \n6. '+team_name[6]+' '
-                                      '\n7. '+team_name[7]+'\n8. '+team_name[8]+'\n9. '+team_name[9]+'\n10. '+team_name[10]+''
-                                      '\n11. '+team_name[11]+'\n12. '+team_name[12]+'\n13. '+team_name[13]+'\n14. '+team_name[14]+''
-                                      '\n15. '+team_name[15]+'\n16. '+team_name[16]+'\n17. '+team_name[17]+'\n18. '+team_name[18]+''
-                                      '\n19. '+team_name[19]+'\n20. '+team_name[20]+'\nReserve\n1. '+team_name[21]+''
-                                      '\n2. '+team_name[22]+'\n3. '+team_name[23]+'\n4. '+team_name[24]+'\n5. '+team_name[25]+'**')
-
-
-
-
+    await team_list_edit.edit(content='**Team List \n1. '+teams[1]+' \n2. '+teams[2]+' \n3. '+teams[3]+' \n4. '+teams[4]+' \n5. '+teams[5]+' \n6. '+teams[6]+' '
+                                      '\n7. '+teams[7]+'\n8. '+teams[8]+'\n9. '+teams[9]+'\n10. '+teams[10]+''
+                                      '\n11. '+teams[11]+'\n12. '+teams[12]+'\n13. '+teams[13]+'\n14. '+teams[14]+''
+                                      '\n15. '+teams[15]+'\n16. '+teams[16]+'\n17. '+teams[17]+'\n18. '+teams[18]+''
+                                      '\n19. '+teams[19]+'\n20. '+teams[20]+'\nReserve\n1. '+teams[21]+''
+                                      '\n2. '+teams[22]+'\n3. '+teams[23]+'\n4. '+teams[24]+'\n5. '+teams[25]+'**')
     await ctx.message.delete()
-
-# Процесс регистрации
-@bot.event
-async def on_message(message):
-
-    await bot.process_commands(message)
-    #данные: сообщение рользователя, получение роли, данные пользователя
-    msg = message.content
-
-
-    role = discord.utils.get(message.guild.roles, name="Cap-chat")
-    Manager = discord.utils.get(message.guild.roles, name="Manager")
-    user = message.author
-    reg_ch = bot.get_channel(819000355613835304)
-    team_list_ch = bot.get_channel(816827996681666650)
-
-
-    global count1
-    global count2
-    global i
-    global open_check
-
-
-
-    if message.channel.id == 819000355613835304 and open_check == True:
-
-        for item in black_list:
-
-            if item == message.author:
-                await message.add_reaction("<:No:818987499766546432>")
-                return
-        if message.author == bot.user or count2 > 4:
-            return
-
-        if count1 <= 17:
-            await user.add_roles(role)
-            await message.add_reaction("<:Yes:818988469891629087>")
-            count1 = count1 + 1
-            team_name[i] = msg
-            i = i + 1
-            if count1 >= 1:
-                team_list_edit = await team_list_ch.fetch_message(team_id)
-                await team_list_edit.edit(content='**Team List\n1. '+team_name[1]+' \n2. '+team_name[2]+' \n3. '+team_name[3]+' \n4. '+team_name[4]+' \n5. '+team_name[5]+' \n6. '+team_name[6]+' '
-                                                         '\n7. '+team_name[7]+'\n8. '+team_name[8]+'\n9. '+team_name[9]+'\n10. '+team_name[10]+''
-                                                         '\n11. '+team_name[11]+'\n12. '+team_name[12]+'\n13. '+team_name[13]+'\n14. '+team_name[14]+''
-                                                         '\n15. '+team_name[15]+'\n16. '+team_name[16]+'\n17. '+team_name[17]+'\n18. '+team_name[18]+''
-                                                         '\n19. '+team_name[19]+'\n20. '+team_name[20]+'\nReserve\n1. '+team_name[21]+''
-                                                         '\n2. '+team_name[22]+'\n3. '+team_name[23]+'\n4. '+team_name[24]+'\n5. '+team_name[25]+'**')
-
-
-        else:
-            await message.add_reaction("<:Res:818992166906888233>")
-            count2 = count2 + 1
-            team_name[i] = msg
-            i = i + 1
-            reserve_edit = await team_list_ch.fetch_message(team_id)
-            await reserve_edit.edit(content='**Team List\n1. '+team_name[1]+' \n2. '+team_name[2]+' \n3. '+team_name[3]+' \n4. '+team_name[4]+' \n5. '+team_name[5]+' \n6. '+team_name[6]+' '
-                                      '\n7. '+team_name[7]+'\n8. '+team_name[8]+'\n9. '+team_name[9]+'\n10. '+team_name[10]+''
-                                      '\n11. '+team_name[11]+'\n12. '+team_name[12]+'\n13. '+team_name[13]+'\n14. '+team_name[14]+''
-                                      '\n15. '+team_name[15]+'\n16. '+team_name[16]+'\n17. '+team_name[17]+'\n18. '+team_name[18]+''
-                                      '\n19. '+team_name[19]+'\n20. '+team_name[20]+'\nReserve\n1. '+team_name[21]+''
-                                      '\n2. '+team_name[22]+'\n3. '+team_name[23]+'\n4. '+team_name[24]+'\n5. '+team_name[25]+'**')
-
-
-
-        if count2 > 4:
-            
-            open_check = False
-            embed_obj = discord.Embed(description='**Регистрация закрыта! @everyone**', colour = discord.Color.from_rgb(199, 0, 0))
-
-            await reg_ch.send(embed=embed_obj)
-            await message.channel.set_permissions(Manager, read_messages=True, send_messages=False)
-
-
-
 
 
 token = os.environ.get('BOT_TOKEN')
 bot.run(token)
+
+
 
 
